2cf99e46c2c233b825d28e4693b56207e8e3bb96
osama-zayed <o.a.s775561590@gmail.com>
2024-07-05 18:19:36 +0300

    Modify FarmerController

---
+++ app/Http/Controllers/api/Collector/CollectorFarmerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Collector;
+
+use App\Http\Controllers\Api\FarmerController;
+use App\Http\Requests\Collector\CollectingRequest;
+use App\Http\Requests\EditUserRequest;
+use App\Http\Requests\LoginRequest;
+use App\Models\CollectingMilkFromCaptivity;
+use Illuminate\Http\Request;
+
+
+class CollectorFarmerController extends FarmerController
+{
+  
+}
