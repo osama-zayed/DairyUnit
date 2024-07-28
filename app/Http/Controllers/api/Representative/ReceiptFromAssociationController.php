66995b34932aad13cd130565cf09443e6b558351
osama-zayed <o.a.s775561590@gmail.com>
2024-07-29 00:32:08 +0300

    New Features

--- app/Http/Controllers/api/Representative/ReceiptFromAssociationController.php
+++ app/Http/Controllers/api/Representative/ReceiptFromAssociationController.php
@@ -8,6 +8,7 @@ use App\Models\AssemblyStore;
 use App\Models\ReceiptFromAssociation;
 use App\Models\TransferToFactory;
 use App\Models\User;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -17,9 +18,39 @@ class ReceiptFromAssociationController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public  function index(Request $request)
     {
-        return self::responseError('jjj');
+        try {
+            return self::responseSuccess(self::getReceiptFromAssociationPaginated($request));
+        } catch (\Throwable $th) {
+            return self::responseError($th);
+        }
+    }
+    public function getReceiptFromAssociationPaginated($request)
+    {
+        $perPage = $request->get('per_page');
+        $page = $request->get('current_page');
+        $query = ReceiptFromAssociation::select(
+            'id',
+            'transfer_to_factory_id',
+            'association_id',
+            'quantity'
+        )
+        ->with('association', 'transferToFactory')
+        ->orderByDesc('id');
+        $ReceiptFromAssociation = $query->paginate($perPage, "", "current_page", $page);
+        return self::formatPaginatedResponse($ReceiptFromAssociation, self::formatReceiptFromAssociationDataForDisplay($ReceiptFromAssociation->items()));
+    }
+    public static function formatReceiptFromAssociationDataForDisplay($ReceiptFromAssociation)
+    {
+        return array_map(function ($ReceiptFromAssociation) {
+            return [
+                'id' => $ReceiptFromAssociation->id,
+                'association_name' => $ReceiptFromAssociation->association->name,
+                'transfer_quantity' => $ReceiptFromAssociation->transferToFactory->quantity,
+                'receipt_quantity' => $ReceiptFromAssociation->quantity,
+            ];
+        }, $ReceiptFromAssociation);
     }
 
     /**
@@ -88,10 +119,16 @@ class ReceiptFromAssociationController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(ReceiptFromAssociation $receiptFromAssociation)
+    public function show($id)
     {
-        //
+        try {
+            $receiptFromAssociation = ReceiptFromAssociation::find($id);
+            return self::responseSuccess(self::formatDataById($receiptFromAssociation));
+        } catch (\Throwable $th) {
+            return self::responseError();
+        }
     }
+
 
     /**
      * Update the specified resource in storage.
@@ -107,5 +144,48 @@ class ReceiptFromAssociationController extends Controller
     public function destroy(ReceiptFromAssociation $receiptFromAssociation)
     {
         //
+    }
+
+    public static function formatDataById($receiptFromAssociation)
+    {
+        $startDateTime = DateTime::createFromFormat('Y-m-d H:i:s', $receiptFromAssociation->start_time_of_collection);
+        $startFormattedDate = $startDateTime->format('d/m/Y');
+        $startFormattedTime = $startDateTime->format('h:i A');
+        $startDayPeriod = self::getDayPeriodArabic($startDateTime->format('A'));
+        $startDayOfWeek = self::getDayOfWeekArabic($startDateTime->format('l'));
+
+        $endDateTime = DateTime::createFromFormat('Y-m-d H:i:s', $receiptFromAssociation->end_time_of_collection);
+        $endFormattedDate = $endDateTime->format('d/m/Y');
+        $endFormattedTime = $endDateTime->format('h:i A');
+        $endDayPeriod = self::getDayPeriodArabic($endDateTime->format('A'));
+        $endDayOfWeek = self::getDayOfWeekArabic($endDateTime->format('l'));
+
+        return [
+            'id' => $receiptFromAssociation->id,
+            'start_time_of_collection' => $receiptFromAssociation->start_time_of_collection,
+            'end_time_of_collection' => $receiptFromAssociation->end_time_of_collection,
+            'start_date' => $startFormattedDate,
+            'end_date' => $endFormattedDate,
+            'start_time' => $startFormattedTime,
+            'end_time' => $endFormattedTime,
+            'start_period' => $startDayPeriod,
+            'end_period' => $endDayPeriod,
+            'start_day' => $startDayOfWeek,
+            'end_day' => $endDayOfWeek,
+            'transfer_to_factory_id' => $receiptFromAssociation->transfer_to_factory_id,
+            'transfer_quantity' => $receiptFromAssociation->transferToFactory->quantity,
+            'receipt_quantity' => $receiptFromAssociation->quantity,
+            'association_id' => $receiptFromAssociation->association->id,
+            'association_name' => $receiptFromAssociation->association->name,
+            'driver_id' => $receiptFromAssociation->driver_id,
+            'driver_name' => $receiptFromAssociation->driver->name,
+            'factory_id' => $receiptFromAssociation->factory_id,
+            'factory_name' => $receiptFromAssociation->factory->name,
+            'package_cleanliness' => $receiptFromAssociation->package_cleanliness,
+            'transport_cleanliness' => $receiptFromAssociation->transport_cleanliness,
+            'driver_personal_hygiene' => $receiptFromAssociation->driver_personal_hygiene,
+            'ac_operation' => $receiptFromAssociation->ac_operation,
+            'notes' => $receiptFromAssociation->notes,
+        ];
     }
 }
