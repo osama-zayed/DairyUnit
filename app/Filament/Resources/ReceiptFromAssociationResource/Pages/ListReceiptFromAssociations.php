ad0762938e8903c646b808502a967021c22de60b
osama-zayed <o.a.s775561590@gmail.com>
2024-07-30 00:42:02 +0300

    New Features

--- app/Filament/Resources/ReceiptFromAssociationResource/Pages/ListReceiptFromAssociations.php
+++ app/Filament/Resources/ReceiptFromAssociationResource/Pages/ListReceiptFromAssociations.php
@@ -13,7 +13,7 @@ class ListReceiptFromAssociations extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
--- app/Filament/Resources/ReceiptFromAssociationResource/Pages/ViewReceiptFromAssociation.php
+++ app/Filament/Resources/ReceiptFromAssociationResource/Pages/ViewReceiptFromAssociation.php
@@ -13,7 +13,7 @@ class ViewReceiptFromAssociation extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            // Actions\EditAction::make(),
         ];
     }
 }
