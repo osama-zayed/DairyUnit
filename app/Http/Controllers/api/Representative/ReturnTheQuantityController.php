53e73c63b748aebcc0a8d7d5b9c9629dd622ea5b
osama-zayed <o.a.s775561590@gmail.com>
2024-07-31 22:49:46 +0300

    New Features

--- app/Http/Controllers/api/Representative/ReturnTheQuantityController.php
+++ app/Http/Controllers/api/Representative/ReturnTheQuantityController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ReturnTheQuantityController\StoreRequest;
 use App\Http\Requests\ReturnTheQuantityController\UpdateRequest;
 use App\Models\AssemblyStore;
-use App\Models\ReceiptFromAssociation;
 use App\Models\ReceiptInvoiceFromStore;
 use App\Models\ReturnTheQuantity;
 use App\Models\User;
@@ -23,7 +22,7 @@ class ReturnTheQuantityController extends Controller
     public  function index(Request $request)
     {
         try {
-            return self::responseSuccess(self::getReceiptFromAssociationPaginated($request));
+            return self::responseSuccess(self::getReturnTheQuantityPaginated($request));
         } catch (\Throwable $th) {
             return self::responseError($th);
         }
@@ -63,7 +62,7 @@ class ReturnTheQuantityController extends Controller
                     ' في مصنع ' . auth('sanctum')->user()->factory->name .
                     ' الكمية المردودة ' . $quantity
             );
-            $userMessage = $userMessage . 'جمعية '.  $association ->name ;
+            $userMessage = $userMessage . 'جمعية ' .  $association->name;
         } else {
             $admin_users = User::where('user_type', "admin")->get();
             foreach ($admin_users as $admin) {
@@ -75,7 +74,7 @@ class ReturnTheQuantityController extends Controller
                         ' الكمية المردودة ' . $quantity
                 );
             }
-            $userMessage = $userMessage . 'المؤاسسة' ;
+            $userMessage = $userMessage . 'المؤاسسة';
         }
 
         $ReturnTheQuantity->save();
@@ -94,7 +93,7 @@ class ReturnTheQuantityController extends Controller
             auth('sanctum')->user(),
             'لقد قمت ب ' .
                 'إنشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                 $userMessage .
+                $userMessage .
                 ' الكمية المردودة ' . $quantity,
         );
 
@@ -103,20 +102,20 @@ class ReturnTheQuantityController extends Controller
     public function update(UpdateRequest $request, $id)
     {
         $ReturnTheQuantity = ReturnTheQuantity::findOrFail($request->input('id'));
-    
+
         $ReturnTheQuantity->return_to = $request->input('return_to');
         $ReturnTheQuantity->defective_quantity_due_to_coagulation = $request->input('defective_quantity_due_to_coagulation');
         $ReturnTheQuantity->defective_quantity_due_to_impurities = $request->input('defective_quantity_due_to_impurities');
         $ReturnTheQuantity->defective_quantity_due_to_density = $request->input('defective_quantity_due_to_density');
         $ReturnTheQuantity->defective_quantity_due_to_acidity = $request->input('defective_quantity_due_to_acidity');
-    
+
         $quantity = $ReturnTheQuantity->defective_quantity_due_to_acidity +
             $ReturnTheQuantity->defective_quantity_due_to_density +
             $ReturnTheQuantity->defective_quantity_due_to_impurities +
             $ReturnTheQuantity->defective_quantity_due_to_coagulation;
-    
+
         $ReturnTheQuantity->notes = $request->input('notes') ?? '';
-    
+
         if ($ReturnTheQuantity->return_to == 'association') {
             $ReturnTheQuantity->association_id = $request->input('association_id');
             $association = User::find($ReturnTheQuantity->association_id);
@@ -127,7 +126,7 @@ class ReturnTheQuantityController extends Controller
                     ' في مصنع ' . auth('sanctum')->user()->factory->name .
                     ' الكمية المردودة ' . $quantity
             );
-            $userMessage = ' والمردودة الى جمعية '. $association->name;
+            $userMessage = ' والمردودة الى جمعية ' . $association->name;
         } else {
             $ReturnTheQuantity->association_id = null;
             $admin_users = User::where('user_type', "admin")->get();
@@ -142,9 +141,9 @@ class ReturnTheQuantityController extends Controller
             }
             $userMessage = ' والمردودة الى المؤاسسة';
         }
-    
+
         $ReturnTheQuantity->save();
-    
+
         self::userActivity(
             'تعديل مرتجع حليب',
             $ReturnTheQuantity,
@@ -154,7 +153,7 @@ class ReturnTheQuantityController extends Controller
                 ' الكمية المردودة ' . $quantity,
             'المندوب'
         );
-    
+
         self::userNotification(
             auth('sanctum')->user(),
             'لقد قمت ب ' .
@@ -162,7 +161,7 @@ class ReturnTheQuantityController extends Controller
                 $userMessage .
                 ' الكمية المردودة ' . $quantity,
         );
-    
+
         return self::responseSuccess([], 'تمت العملية بنجاح');
     }
     /**
@@ -170,15 +169,16 @@ class ReturnTheQuantityController extends Controller
      */
     public function show($id)
     {
-        // try {
-        //     $user = auth('sanctum')->user();
-        //     $receiptFromAssociation = ReceiptFromAssociation::where('id', $id)
-        //         ->where('user_id',  $user->id)
-        //         ->first();
-        //     return self::responseSuccess(self::formatDataById($receiptFromAssociation));
-        // } catch (\Throwable $th) {
-        //     return self::responseError($th);
-        // }
+        try {
+            $user = auth('sanctum')->user();
+            $ReturnTheQuantity = ReturnTheQuantity::where('id', $id)
+                ->where('user_id',  $user->id)
+                ->where('factory_id',  $user->factory_id)
+                ->first();
+            return self::responseSuccess(self::formatDataById($ReturnTheQuantity));
+        } catch (\Throwable $th) {
+            return self::responseError($th);
+        }
     }
 
 
@@ -191,80 +191,83 @@ class ReturnTheQuantityController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ReceiptFromAssociation $receiptFromAssociation)
+    public function destroy(ReturnTheQuantity $ReturnTheQuantity)
     {
         //
     }
 
-    public function getReceiptFromAssociationPaginated($request)
+    public function getReturnTheQuantityPaginated($request)
     {
         $perPage = $request->get('per_page');
         $page = $request->get('current_page');
+        $returnTo = $request->get('return_to');
         $user = auth('sanctum')->user();
-        $query = ReceiptFromAssociation::select(
+        $query = ReturnTheQuantity::select(
             'id',
-            'transfer_to_factory_id',
             'association_id',
-            'quantity'
+            'return_to',
+            'defective_quantity_due_to_coagulation',
+            'defective_quantity_due_to_impurities',
+            'defective_quantity_due_to_density',
+            'defective_quantity_due_to_acidity',
+            'user_id',
         )
-            ->with('association', 'ReturnTheQuantity')
             ->where('user_id',  $user->id)
+            ->where('factory_id',  $user->factory_id)
+            ->with('association')
             ->orderByDesc('id');
-        $ReceiptFromAssociation = $query->paginate($perPage, "", "current_page", $page);
-        return self::formatPaginatedResponse($ReceiptFromAssociation, self::formatReceiptFromAssociationDataForDisplay($ReceiptFromAssociation->items()));
+        if ($returnTo == 'association') {
+            $query->where('association_id', '!=', null);
+        } else {
+            $query->where('association_id',  null);
+        }
+        $ReturnTheQuantity = $query->paginate($perPage, "", "current_page", $page);
+        return self::formatPaginatedResponse($ReturnTheQuantity, self::formatReturnTheQuantityDataForDisplay($ReturnTheQuantity->items()));
     }
-    public static function formatReceiptFromAssociationDataForDisplay($ReceiptFromAssociation)
+    public static function formatReturnTheQuantityDataForDisplay($ReturnTheQuantity)
     {
-        return array_map(function ($ReceiptFromAssociation) {
+        return array_map(function ($ReturnTheQuantity) {
             return [
-                'id' => $ReceiptFromAssociation->id,
-                'association_name' => $ReceiptFromAssociation->association->name,
-                'transfer_quantity' => $ReceiptFromAssociation->ReturnTheQuantity->quantity,
-                'receipt_quantity' => $ReceiptFromAssociation->quantity,
+                'id' => $ReturnTheQuantity->id,
+                'quantity' =>  $ReturnTheQuantity->defective_quantity_due_to_acidity +
+                    $ReturnTheQuantity->defective_quantity_due_to_density +
+                    $ReturnTheQuantity->defective_quantity_due_to_impurities +
+                    $ReturnTheQuantity->defective_quantity_due_to_coagulation,
+                'return_to' => ($ReturnTheQuantity->return_to == "association") ? 'مردود الى جمعية ' . $ReturnTheQuantity->association->name :
+                    'مردود الى المؤاسسة'
             ];
-        }, $ReceiptFromAssociation);
+        }, $ReturnTheQuantity);
     }
 
-    public static function formatDataById($receiptFromAssociation)
+    public static function formatDataById($ReturnTheQuantity)
     {
-        $startDateTime = DateTime::createFromFormat('Y-m-d H:i:s', $receiptFromAssociation->start_time_of_collection);
-        $startFormattedDate = $startDateTime->format('d/m/Y');
-        $startFormattedTime = $startDateTime->format('h:i A');
-        $startDayPeriod = self::getDayPeriodArabic($startDateTime->format('A'));
-        $startDayOfWeek = self::getDayOfWeekArabic($startDateTime->format('l'));
-
-        $endDateTime = DateTime::createFromFormat('Y-m-d H:i:s', $receiptFromAssociation->end_time_of_collection);
-        $endFormattedDate = $endDateTime->format('d/m/Y');
-        $endFormattedTime = $endDateTime->format('h:i A');
-        $endDayPeriod = self::getDayPeriodArabic($endDateTime->format('A'));
-        $endDayOfWeek = self::getDayOfWeekArabic($endDateTime->format('l'));
+        $DateTime = DateTime::createFromFormat('Y-m-d H:i:s', $ReturnTheQuantity->created_at);
+        $FormattedDate = $DateTime->format('d/m/Y');
+        $FormattedTime = $DateTime->format('h:i A');
+        $DayPeriod = self::getDayPeriodArabic($DateTime->format('A'));
+        $DayOfWeek = self::getDayOfWeekArabic($DateTime->format('l'));
 
         return [
-            'id' => $receiptFromAssociation->id,
-            'start_time_of_collection' => $receiptFromAssociation->start_time_of_collection,
-            'end_time_of_collection' => $receiptFromAssociation->end_time_of_collection,
-            'start_date' => $startFormattedDate,
-            'end_date' => $endFormattedDate,
-            'start_time' => $startFormattedTime,
-            'end_time' => $endFormattedTime,
-            'start_period' => $startDayPeriod,
-            'end_period' => $endDayPeriod,
-            'start_day' => $startDayOfWeek,
-            'end_day' => $endDayOfWeek,
-            'transfer_to_factory_id' => $receiptFromAssociation->transfer_to_factory_id,
-            'transfer_quantity' => $receiptFromAssociation->ReturnTheQuantity->quantity,
-            'receipt_quantity' => $receiptFromAssociation->quantity,
-            'association_id' => $receiptFromAssociation->association->id,
-            'association_name' => $receiptFromAssociation->association->name,
-            'driver_id' => $receiptFromAssociation->driver_id,
-            'driver_name' => $receiptFromAssociation->driver->name,
-            'factory_id' => $receiptFromAssociation->factory_id,
-            'factory_name' => $receiptFromAssociation->factory->name,
-            'package_cleanliness' => $receiptFromAssociation->package_cleanliness,
-            'transport_cleanliness' => $receiptFromAssociation->transport_cleanliness,
-            'driver_personal_hygiene' => $receiptFromAssociation->driver_personal_hygiene,
-            'ac_operation' => $receiptFromAssociation->ac_operation,
-            'notes' => $receiptFromAssociation->notes,
+            'id' => $ReturnTheQuantity->id,
+            'date' => $FormattedDate,
+            'time' => $FormattedTime,
+            'period' => $DayPeriod,
+            'day' => $DayOfWeek,
+            'quantity' =>  $ReturnTheQuantity->defective_quantity_due_to_acidity +
+                $ReturnTheQuantity->defective_quantity_due_to_density +
+                $ReturnTheQuantity->defective_quantity_due_to_impurities +
+                $ReturnTheQuantity->defective_quantity_due_to_coagulation,
+            'return' => ($ReturnTheQuantity->return_to == "association") ? 'مردود الى جمعية ' . $ReturnTheQuantity->association->name :
+                'مردود الى المؤاسسة',
+            'return_to' => $ReturnTheQuantity->return_to,
+
+            'association_id' => $ReturnTheQuantity->association_id,
+            'defective_quantity_due_to_coagulation' => $ReturnTheQuantity->defective_quantity_due_to_coagulation,
+            'defective_quantity_due_to_impurities' => $ReturnTheQuantity->defective_quantity_due_to_impurities,
+            'defective_quantity_due_to_density' => $ReturnTheQuantity->defective_quantity_due_to_density,
+            'defective_quantity_due_to_acidity' => $ReturnTheQuantity->defective_quantity_due_to_acidity,
+
+            'notes' => $ReturnTheQuantity->notes,
         ];
     }
 }
