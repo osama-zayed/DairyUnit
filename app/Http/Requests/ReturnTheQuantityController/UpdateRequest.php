fe6e19eed2409544b44d8b58836ce6fe0792da7f
osama-zayed <o.a.s775561590@gmail.com>
2024-07-31 22:20:07 +0300

    New Features

--- app/Http/Requests/ReturnTheQuantityController/UpdateRequest.php
+++ app/Http/Requests/ReturnTheQuantityController/UpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\ReturnTheQuantityController;
 
 use App\Models\ReceiptFromAssociation;
+use App\Models\ReturnTheQuantity;
 use App\Models\TransferToFactory;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -26,53 +27,12 @@ class UpdateRequest extends FormRequest
                 'required',
                 'integer',
             ],
-            'start_time_of_collection' => [
-                'required',
-                'date_format:Y-m-d H:i:s',
-                function ($attribute, $value, $fail) {
-                    $requestDateTime = \Carbon\Carbon::parse($value);
-                    $now = \Carbon\Carbon::now();
-                    $twoDaysAgo = \Carbon\Carbon::now()->subDays(2);
-
-                    if ($requestDateTime->greaterThan($now)) {
-                        $fail('يجب أن لا يكون تاريخ ووقت بدء الفحص بعد الوقت الحالي.');
-                    }
-
-                    if ($requestDateTime->lt($twoDaysAgo)) {
-                        $fail('يجب أن لا يكون تاريخ ووقت بدء الفحص قبل يومين من الوقت الحالي.');
-                    }
-                },
-            ],
-            'end_time_of_collection' => [
-                'required',
-                'date_format:Y-m-d H:i:s',
-                function ($attribute, $value, $fail) {
-                    $end_time = \Carbon\Carbon::parse($value);
-
-                    $now = \Carbon\Carbon::now();
-                    $twoDaysAgo = \Carbon\Carbon::now()->subDays(2);
-
-                    if ($end_time->greaterThan($now)) {
-                        $fail('يجب أن لا يكون تاريخ ووقت انتهاء الفحص بعد الوقت الحالي');
-                    }
-
-                    if ($end_time->lt($twoDaysAgo)) {
-                        $fail('يجب أن لا يكون تاريخ ووقت انتهاء الفحص قبل يومين من الوقت الحالي');
-                    }
-                },
-            ],
-            // 'transfer_to_factory_id' => [
-            //     'exists:transfer_to_factories,id',
-            // ],
-            'quantity' => 'required|numeric|min:1',
-            'package_cleanliness' => 'required|in:clean,somewhat_clean,not_clean',
-            'transport_cleanliness' => 'required|in:clean,somewhat_clean,not_clean',
-            'driver_personal_hygiene' => 'required|in:clean,somewhat_clean,not_clean',
-            'ac_operation' => 'required|in:on,off,not_available',
-            // 'defective_quantity_due_to_coagulation' => 'required|numeric|min:0',
-            // 'defective_quantity_due_to_impurities' => 'required|numeric|min:0',
-            // 'defective_quantity_due_to_density' => 'required|numeric|min:0',
-            // 'defective_quantity_due_to_acidity' => 'required|numeric|min:0',
+            'association_id' => 'nullable|exists:users,id',
+            'return_to' => 'required|in:institution,association',
+            'defective_quantity_due_to_coagulation' => 'required|numeric|min:0',
+            'defective_quantity_due_to_impurities' => 'required|numeric|min:0',
+            'defective_quantity_due_to_density' => 'required|numeric|min:0',
+            'defective_quantity_due_to_acidity' => 'required|numeric|min:0',
             'notes' => 'nullable',
         ];
     }
@@ -87,62 +47,43 @@ class UpdateRequest extends FormRequest
     {
         $validator->after(function ($validator) {
             $user = auth('sanctum')->user();
-            $receiptFromAssociation = ReceiptFromAssociation::find($this->input('id'));
-            if ($receiptFromAssociation) {
-                if ($receiptFromAssociation->user_id != $user->id) {
+            $ReturnTheQuantity = ReturnTheQuantity::find($this->input('id'));
+            if ($ReturnTheQuantity) {
+                if ($ReturnTheQuantity->user_id != $user->id) {
                     $validator->errors()->add('id', 'لم تقم انت باضافة هذه العملية');
                 }
-                $createdAt = $receiptFromAssociation->created_at;
+                $createdAt = $ReturnTheQuantity->created_at;
                 $now = now();
                 $diffInHours = $now->diffInHours($createdAt);
                 if ($diffInHours >= 2) {
                     $validator->errors()->add('id', 'لا يمكن تعديل السجل بعد مرور ساعتين من إضافته');
                 }
 
-                $lastTransferToFactory = TransferToFactory::where('factory_id', $user->factory_id)
+                $lastReceiptFromAssociation = ReceiptFromAssociation::where('factory_id', $user->factory_id)
+                    ->where('user_id', $user->id)
                     ->select('created_at')
                     ->latest()
                     ->first();
 
-                $lastReceiptFromAssociation = ReceiptFromAssociation::where('user_id', $user->id)
+                $lastReturnTheQuantity = ReturnTheQuantity::where('user_id', $user->id)
                     ->select('created_at')
                     ->latest()
                     ->first();
 
-                if ($receiptFromAssociation && (
-                    ($receiptFromAssociation->created_at < $lastReceiptFromAssociation->created_at) ||
-                    ($receiptFromAssociation->created_at < $lastTransferToFactory->created_at)
+                if ($ReturnTheQuantity && (
+                    ($ReturnTheQuantity->created_at < $lastReturnTheQuantity->created_at) ||
+                    ($ReturnTheQuantity->created_at < $lastReceiptFromAssociation->created_at)
                 )) {
                     $validator->errors()->add('id', 'لا يمكن التعديل لأنه حصل عملية في وقت لاحق');
                 }
+                $returnTo = $this->input('return_to');
+                $receiptFromAssociationId = $this->input('association_id');
 
-                $quantity = $this->input('quantity');
-                $startTimeOfCollection = $this->input('start_time_of_collection');
-                $endTimeOfCollection = $this->input('end_time_of_collection');
-                
-                $start = \Carbon\Carbon::parse($startTimeOfCollection);
-                $end = \Carbon\Carbon::parse($endTimeOfCollection);
-                
-                if ($end->lessThan($start)) {
-                    $validator->errors()->add('end_time_of_collection', 'يجب أن يكون تاريخ ووقت انتهاء الفحص بعد تاريخ ووقت بدء الفحص.');
+                if ($returnTo == 'association' && is_null($receiptFromAssociationId)) {
+                    $validator->errors()->add('association_id', 'معرف الجمعية مطلوب');
                 }
-                
-                $transferToFactoryId = $receiptFromAssociation->transfer_to_factory_id;
-                // تحقق من وجود معرف التحويل
-                if ($transferToFactoryId) {
-                    $transferToFactory = TransferToFactory::find($transferToFactoryId);
-
-                    if ($transferToFactory) {
-                        // if ($transferToFactory->status) {
-                        //     $validator->errors()->add('', 'لقد تم تاكيد استلام عملية التحويل من قبل');
-                        // }
-                        if ($quantity > $transferToFactory->quantity) {
-                            $validator->errors()->add('quantity', 'لا يمكن أن تكون الكمية في الاستلام أكبر من الكمية في التحويل');
-                        }
-                    }
-                }
-            }else{
-                $validator->errors()->add('id', 'عملية الاستلام غير موجودة');
+            } else {
+                $validator->errors()->add('id', 'العملية غير موجودة');
             }
         });
     }
@@ -155,34 +96,21 @@ class UpdateRequest extends FormRequest
             'id.required' => 'معرف  العملية مطلوب',
             'id.exists' => ' العملية المحددة غير موجودة',
             'id.integer' => 'معرف العملية يجب ان يكون رقم ',
-            'date_and_time.required' => 'تاريخ ووقت الجمع مطلوب',
-            'date_and_time.date_format' => 'يجب أن يكون تاريخ ووقت الجمع صالحًا',
-            'transfer_to_factory_id.exists' => 'معرف عملية التحويل المحدده غير موجوده',
-            'start_time_of_collection.required' => 'وقت بداية الجمع مطلوب',
-            'end_time_of_collection.required' => 'وقت نهاية الجمع مطلوب',
-            'quantity.required' => 'عدد العبوات مطلوب',
-            'quantity.numeric' => 'عدد العبوات يجب أن يكون رقميًا',
-            'quantity.min' => 'عدد العبوات يجب أن يكون على الأقل 1',
-            'package_cleanliness.required' => 'نظافة العبوات مطلوبة',
-            'package_cleanliness.in' => 'نظافة العبوات يجب أن تكون "clean" أو "somewhat_clean" أو "not_clean"',
-            'transport_cleanliness.required' => 'نظافة النقل مطلوبة',
-            'transport_cleanliness.in' => 'نظافة النقل يجب أن تكون "clean" أو "somewhat_clean" أو "not_clean"',
-            'driver_personal_hygiene.required' => 'نظافة الشخصية للسائق مطلوبة',
-            'driver_personal_hygiene.in' => 'نظافة الشخصية للسائق يجب أن تكون "clean" أو "somewhat_clean" أو "not_clean"',
-            'ac_operation.required' => 'حالة نظام التكييف مطلوبة',
-            'ac_operation.in' => 'حالة نظام التكييف يجب أن تكون "on" أو "off" أو "not_available"',
-            // 'defective_quantity_due_to_coagulation.required' => 'الكمية المعيبة بسبب التخثر مطلوبة',
-            // 'defective_quantity_due_to_coagulation.numeric' => 'الكمية المعيبة بسبب التخثر يجب أن تكون رقمية',
-            // 'defective_quantity_due_to_coagulation.min' => 'الكمية المعيبة بسبب التخثر يجب أن تكون على الأقل 0',
-            // 'defective_quantity_due_to_impurities.required' => 'الكمية المعيبة بسبب الشوائب مطلوبة',
-            // 'defective_quantity_due_to_impurities.numeric' => 'الكمية المعيبة بسبب الشوائب يجب أن تكون رقمية',
-            // 'defective_quantity_due_to_impurities.min' => 'الكمية المعيبة بسبب الشوائب يجب أن تكون على الأقل 0',
-            // 'defective_quantity_due_to_density.required' => 'الكمية المعيبة بسبب الكثافة مطلوبة',
-            // 'defective_quantity_due_to_density.numeric' => 'الكمية المعيبة بسبب الكثافة يجب أن تكون رقمية',
-            // 'defective_quantity_due_to_density.min' => 'الكمية المعيبة بسبب الكثافة يجب أن تكون على الأقل 0',
-            // 'defective_quantity_due_to_acidity.required' => 'الكمية المعيبة بسبب الحموضة مطلوبة',
-            // 'defective_quantity_due_to_acidity.numeric' => 'الكمية المعيبة بسبب الحموضة يجب أن تكون رقمية',
-            // 'defective_quantity_due_to_acidity.min' => 'الكمية المعيبة بسبب الحموضة يجب أن تكون على الأقل 0',
+            'return_to.required' => 'الجهة المحول لها مطلوبة',
+            'return_to.in' => 'الجهة المحول لها يجب ان تكون institution او association',
+            'association_id.exists' => 'معرف الجمعية مطلوب',
+            'defective_quantity_due_to_coagulation.required' => 'الكمية التالفة بسبب التخثر مطلوبة',
+            'defective_quantity_due_to_coagulation.numeric' => 'الكمية التالفة بسبب التخثر يجب أن تكون رقمية',
+            'defective_quantity_due_to_coagulation.min' => 'الكمية التالفة بسبب التخثر يجب أن تكون على الأقل 0',
+            'defective_quantity_due_to_impurities.required' => 'الكمية التالفة بسبب الشوائب مطلوبة',
+            'defective_quantity_due_to_impurities.numeric' => 'الكمية التالفة بسبب الشوائب يجب أن تكون رقمية',
+            'defective_quantity_due_to_impurities.min' => 'الكمية التالفة بسبب الشوائب يجب أن تكون على الأقل 0',
+            'defective_quantity_due_to_density.required' => 'الكمية التالفة بسبب الكثافة مطلوبة',
+            'defective_quantity_due_to_density.numeric' => 'الكمية التالفة بسبب الكثافة يجب أن تكون رقمية',
+            'defective_quantity_due_to_density.min' => 'الكمية التالفة بسبب الكثافة يجب أن تكون على الأقل 0',
+            'defective_quantity_due_to_acidity.required' => 'الكمية التالفة بسبب الحموضة مطلوبة',
+            'defective_quantity_due_to_acidity.numeric' => 'الكمية التالفة بسبب الحموضة يجب أن تكون رقمية',
+            'defective_quantity_due_to_acidity.min' => 'الكمية التالفة بسبب الحموضة يجب أن تكون على الأقل 0',
         ];
     }
 
