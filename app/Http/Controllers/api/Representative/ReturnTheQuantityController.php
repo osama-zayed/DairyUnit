1b357f5df7db2f372cd4e81f3207bc34790321f0
osama-zayed <o.a.s775561590@gmail.com>
2024-08-04 01:56:19 +0300

    New Features

--- app/Http/Controllers/api/Representative/ReturnTheQuantityController.php
+++ app/Http/Controllers/api/Representative/ReturnTheQuantityController.php
@@ -34,135 +34,147 @@ class ReturnTheQuantityController extends Controller
      */
     public function store(StoreRequest $request)
     {
+        try {
+            DB::transaction(function () use ($request) {
+                $ReturnTheQuantity = new ReturnTheQuantity();
 
-        $ReturnTheQuantity = new ReturnTheQuantity();
+                $ReturnTheQuantity->user_id = auth('sanctum')->user()->id;
+                $ReturnTheQuantity->factory_id = auth('sanctum')->user()->factory_id;
+                $ReturnTheQuantity->return_to = $request->input('return_to');
+                $ReturnTheQuantity->defective_quantity_due_to_coagulation = $request->input('defective_quantity_due_to_coagulation');
+                $ReturnTheQuantity->defective_quantity_due_to_impurities = $request->input('defective_quantity_due_to_impurities');
+                $ReturnTheQuantity->defective_quantity_due_to_density = $request->input('defective_quantity_due_to_density');
+                $ReturnTheQuantity->defective_quantity_due_to_acidity = $request->input('defective_quantity_due_to_acidity');
 
-        $ReturnTheQuantity->user_id = auth('sanctum')->user()->id;
-        $ReturnTheQuantity->factory_id = auth('sanctum')->user()->factory_id;
-        $ReturnTheQuantity->return_to = $request->input('return_to');
-        $ReturnTheQuantity->defective_quantity_due_to_coagulation = $request->input('defective_quantity_due_to_coagulation');
-        $ReturnTheQuantity->defective_quantity_due_to_impurities = $request->input('defective_quantity_due_to_impurities');
-        $ReturnTheQuantity->defective_quantity_due_to_density = $request->input('defective_quantity_due_to_density');
-        $ReturnTheQuantity->defective_quantity_due_to_acidity = $request->input('defective_quantity_due_to_acidity');
+                $quantity = $ReturnTheQuantity->defective_quantity_due_to_acidity +
+                    $ReturnTheQuantity->defective_quantity_due_to_density +
+                    $ReturnTheQuantity->defective_quantity_due_to_impurities +
+                    $ReturnTheQuantity->defective_quantity_due_to_coagulation;
 
-        $quantity = $ReturnTheQuantity->defective_quantity_due_to_acidity +
-            $ReturnTheQuantity->defective_quantity_due_to_density +
-            $ReturnTheQuantity->defective_quantity_due_to_impurities +
-            $ReturnTheQuantity->defective_quantity_due_to_coagulation;
+                $ReturnTheQuantity->notes = $request->input('notes') ?? '';
+                $userMessage = ' والمردودة الى ';
+                if ($ReturnTheQuantity->return_to == 'association') {
+                    $ReturnTheQuantity->association_id = $request->input('association_id');
+                    $association = User::find($ReturnTheQuantity->association_id);
+                    self::userNotification(
+                        $association,
+                        'تم إنشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
+                            ' من قبل المندوب ' . auth('sanctum')->user()->name .
+                            ' في مصنع ' . auth('sanctum')->user()->factory->name .
+                            ' الكمية المردودة ' . $quantity
+                    );
+                    $userMessage = $userMessage . 'جمعية ' .  $association->name;
+                } else {
+                    $admin_users = User::where('user_type', "admin")->get();
+                    foreach ($admin_users as $admin) {
+                        self::userNotification(
+                            $admin,
+                            'تم إنشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
+                                ' من قبل المندوب ' . auth('sanctum')->user()->name .
+                                ' في مصنع ' . auth('sanctum')->user()->factory->name .
+                                ' الكمية المردودة ' . $quantity
+                        );
+                    }
+                    $userMessage = $userMessage . 'المؤسسة';
+                }
+                $ReturnTheQuantity->quantity = $quantity;
+                $ReturnTheQuantity->save();
 
-        $ReturnTheQuantity->notes = $request->input('notes') ?? '';
-        $userMessage = ' والمردودة الى ';
-        if ($ReturnTheQuantity->return_to == 'association') {
-            $ReturnTheQuantity->association_id = $request->input('association_id');
-            $association = User::find($ReturnTheQuantity->association_id);
-            self::userNotification(
-                $association,
-                'تم إنشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                    ' من قبل المندوب ' . auth('sanctum')->user()->name .
-                    ' في مصنع ' . auth('sanctum')->user()->factory->name .
-                    ' الكمية المردودة ' . $quantity
-            );
-            $userMessage = $userMessage . 'جمعية ' .  $association->name;
-        } else {
-            $admin_users = User::where('user_type', "admin")->get();
-            foreach ($admin_users as $admin) {
-                self::userNotification(
-                    $admin,
-                    'تم إنشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                        ' من قبل المندوب ' . auth('sanctum')->user()->name .
+                self::userActivity(
+                    'مرتجع حليب ',
+                    $ReturnTheQuantity,
+                    ' بانشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
                         ' في مصنع ' . auth('sanctum')->user()->factory->name .
-                        ' الكمية المردودة ' . $quantity
+                        $userMessage .
+                        ' الكمية المردودة ' . $quantity,
+                    'المندوب'
                 );
-            }
-            $userMessage = $userMessage . 'المؤسسة';
+
+                self::userNotification(
+                    auth('sanctum')->user(),
+                    'لقد قمت ب ' .
+                        'إنشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
+                        $userMessage .
+                        ' الكمية المردودة ' . $quantity,
+                );
+            });
+
+            return self::responseSuccess([], 'تمت العملية بنجاح');
+        } catch (\Exception $e) {
+            return self::responseError('حدث خطأ أثناء تنفيذ العملية');
         }
-        $ReturnTheQuantity->quantity = $quantity;
-        $ReturnTheQuantity->save();
-
-        self::userActivity(
-            'مرتجع حليب ',
-            $ReturnTheQuantity,
-            ' بانشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                ' في مصنع ' . auth('sanctum')->user()->factory->name .
-                $userMessage .
-                ' الكمية المردودة ' . $quantity,
-            'المندوب'
-        );
-
-        self::userNotification(
-            auth('sanctum')->user(),
-            'لقد قمت ب ' .
-                'إنشاء مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                $userMessage .
-                ' الكمية المردودة ' . $quantity,
-        );
-
-        return self::responseSuccess([], 'تمت العملية بنجاح');
     }
     public function update(UpdateRequest $request, $id)
     {
-        $ReturnTheQuantity = ReturnTheQuantity::findOrFail($request->input('id'));
+        try {
 
-        $ReturnTheQuantity->return_to = $request->input('return_to');
-        $ReturnTheQuantity->defective_quantity_due_to_coagulation = $request->input('defective_quantity_due_to_coagulation');
-        $ReturnTheQuantity->defective_quantity_due_to_impurities = $request->input('defective_quantity_due_to_impurities');
-        $ReturnTheQuantity->defective_quantity_due_to_density = $request->input('defective_quantity_due_to_density');
-        $ReturnTheQuantity->defective_quantity_due_to_acidity = $request->input('defective_quantity_due_to_acidity');
+            DB::transaction(function () use ($request, $id) {
+                $ReturnTheQuantity = ReturnTheQuantity::findOrFail($request->input('id'));
 
-        $quantity = $ReturnTheQuantity->defective_quantity_due_to_acidity +
-            $ReturnTheQuantity->defective_quantity_due_to_density +
-            $ReturnTheQuantity->defective_quantity_due_to_impurities +
-            $ReturnTheQuantity->defective_quantity_due_to_coagulation;
+                $ReturnTheQuantity->return_to = $request->input('return_to');
+                $ReturnTheQuantity->defective_quantity_due_to_coagulation = $request->input('defective_quantity_due_to_coagulation');
+                $ReturnTheQuantity->defective_quantity_due_to_impurities = $request->input('defective_quantity_due_to_impurities');
+                $ReturnTheQuantity->defective_quantity_due_to_density = $request->input('defective_quantity_due_to_density');
+                $ReturnTheQuantity->defective_quantity_due_to_acidity = $request->input('defective_quantity_due_to_acidity');
 
-        $ReturnTheQuantity->notes = $request->input('notes') ?? '';
+                $quantity = $ReturnTheQuantity->defective_quantity_due_to_acidity +
+                    $ReturnTheQuantity->defective_quantity_due_to_density +
+                    $ReturnTheQuantity->defective_quantity_due_to_impurities +
+                    $ReturnTheQuantity->defective_quantity_due_to_coagulation;
 
-        if ($ReturnTheQuantity->return_to == 'association') {
-            $ReturnTheQuantity->association_id = $request->input('association_id');
-            $association = User::find($ReturnTheQuantity->association_id);
-            self::userNotification(
-                $association,
-                'تم تعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                    ' من قبل المندوب ' . auth('sanctum')->user()->name .
-                    ' في مصنع ' . auth('sanctum')->user()->factory->name .
-                    ' الكمية المردودة ' . $quantity
-            );
-            $userMessage = ' والمردودة الى جمعية ' . $association->name;
-        } else {
-            $ReturnTheQuantity->association_id = null;
-            $admin_users = User::where('user_type', "admin")->get();
-            foreach ($admin_users as $admin) {
-                self::userNotification(
-                    $admin,
-                    'تم تعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                        ' من قبل المندوب ' . auth('sanctum')->user()->name .
+                $ReturnTheQuantity->notes = $request->input('notes') ?? '';
+
+                if ($ReturnTheQuantity->return_to == 'association') {
+                    $ReturnTheQuantity->association_id = $request->input('association_id');
+                    $association = User::find($ReturnTheQuantity->association_id);
+                    self::userNotification(
+                        $association,
+                        'تم تعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
+                            ' من قبل المندوب ' . auth('sanctum')->user()->name .
+                            ' في مصنع ' . auth('sanctum')->user()->factory->name .
+                            ' الكمية المردودة ' . $quantity
+                    );
+                    $userMessage = ' والمردودة الى جمعية ' . $association->name;
+                } else {
+                    $ReturnTheQuantity->association_id = null;
+                    $admin_users = User::where('user_type', "admin")->get();
+                    foreach ($admin_users as $admin) {
+                        self::userNotification(
+                            $admin,
+                            'تم تعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
+                                ' من قبل المندوب ' . auth('sanctum')->user()->name .
+                                ' في مصنع ' . auth('sanctum')->user()->factory->name .
+                                ' الكمية المردودة ' . $quantity
+                        );
+                    }
+                    $userMessage = ' والمردودة الى المؤسسة';
+                }
+                $ReturnTheQuantity->quantity = $quantity;
+                $ReturnTheQuantity->save();
+
+                self::userActivity(
+                    'تعديل مرتجع حليب',
+                    $ReturnTheQuantity,
+                    ' بتعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
                         ' في مصنع ' . auth('sanctum')->user()->factory->name .
-                        ' الكمية المردودة ' . $quantity
+                        $userMessage .
+                        ' الكمية المردودة ' . $quantity,
+                    'المندوب'
                 );
-            }
-            $userMessage = ' والمردودة الى المؤسسة';
+
+                self::userNotification(
+                    auth('sanctum')->user(),
+                    'لقد قمت ب ' .
+                        'تعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
+                        $userMessage .
+                        ' الكمية المردودة ' . $quantity,
+                );
+            });
+
+            return self::responseSuccess([], 'تمت العملية بنجاح');
+        } catch (\Exception $e) {
+            return self::responseError('حدث خطأ أثناء تنفيذ العملية');
         }
-        $ReturnTheQuantity->quantity = $quantity;
-        $ReturnTheQuantity->save();
-
-        self::userActivity(
-            'تعديل مرتجع حليب',
-            $ReturnTheQuantity,
-            ' بتعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                ' في مصنع ' . auth('sanctum')->user()->factory->name .
-                $userMessage .
-                ' الكمية المردودة ' . $quantity,
-            'المندوب'
-        );
-
-        self::userNotification(
-            auth('sanctum')->user(),
-            'لقد قمت ب ' .
-                'تعديل مرتجع حليب برقم ' . $ReturnTheQuantity->id .
-                $userMessage .
-                ' الكمية المردودة ' . $quantity,
-        );
-
-        return self::responseSuccess([], 'تمت العملية بنجاح');
     }
     /**
      * Display the specified resource.
