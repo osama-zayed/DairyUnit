de0629a35a2330ea87ee7c14a6a845a4b8a539bc
osama-zayed <o.a.s775561590@gmail.com>
2024-08-17 01:48:41 +0300

    last commit

--- database/migrations/2024_07_29_003533_add_column_user_id_to_receipt_from_associations_table.php
+++ database/migrations/2024_07_29_003533_add_column_user_id_to_receipt_from_associations_table.php
@@ -22,7 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('associations', function (Blueprint $table) {
-            //
+            $table->dropColumn('user_id');
+
         });
     }
 };
--- database/migrations/2024_08_01_231911_add_quantity_colomn_to_returns.php
+++ database/migrations/2024_08_01_231911_add_quantity_colomn_to_returns.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('returns', function (Blueprint $table) {
-            //
+            $table->dropColumn('quantity');
         });
     }
 };
